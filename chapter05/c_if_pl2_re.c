cae19f7dcbcda1b2d3fb7506a98564dec10d382d
cheonsiwon <112988973+cheonsiwon@users.noreply.github.com>
2022-10-17 10:43:19 +0900

    Add files via upload

--- chapter05/c_if_pl2_re.c
+++ chapter05/c_if_pl2_re.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
 
 int main(void) {
+    //1. 사용자로부터 점수 입력
     int score;
     char grade;
-
     printf("점수: ");
     scanf("%d", &score);
 
     if(score >= 0 && score <= 100) {
+        //2. 점수로부터 등급(a,b,c)판단
         // 점수 : 0~100
-        if(score >= 91) {
+        if(score >= 91) {               //최고score: 100점(91~100)
             grade = 'A';
-        } else if( score >= 81) {
+        } else if( score >= 81) {       //최고score: 90점(81~90)
             grade = 'B';
-        } else if( score >= 71) {
+        } else if( score >= 71) {       //최고score: 80점(71~80)
             grade = 'C';
-        } else if(score >= 61) {
+        } else if(score >= 61) {        //최고score: 70점(61~70)
             grade = 'D';
-        } else {
+        } else {                        //최고score: 60점(0~60)
             grade = 'F';
         }
+        //3. 점수와 등급을 출력
         printf("학생의 점수는 %d점으로 %c학점입니다.", score, grade);
     } else {
         // 점수 : 0점 미만 또는 100점 초과
